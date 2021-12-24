a8e6758c1dcdc0572082f3d79769d028dbfbb1b6
jayatew <112473@emmauscollege.nl>
2021-12-24 17:45:50 +0000

    api.letsgoooooooo

--- db/create.sql
+++ db/create.sql
@@ -8,7 +8,14 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC(10, 2),
+  merk_id INTEGER
+);
+
+DROP TABLE IF EXISTS merk;
+CREATE TABLE merk(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(25)
 );
 
 DROP TABLE IF EXISTS ratings; 
@@ -43,16 +50,16 @@ CREATE TABLE kleuren(
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Nike air force 1', 'Nike air force 1 zijn een van de populairste schoenen van de afgelopen jaren. Ze zijn een goede basic die bij elke outfit past.', '816905633-0', 100.00);
-insert into products (name, description, code, price) values ('Nike jordans 1', 'Deze collectie van Nike is gemaakt met Micheal Jorden, een van de bekendste basketballers ter wereld.', '077030122-3', 120.00);
-insert into products (name, description, code, price) values ('New Balance 550', 'Deze New Balance schoenen werden voor het eerst uitgebracht in 1989, maar heb een nu een kompleet nieuwe look gekregen.', '445924201-X', 220.00);
-insert into products (name, description, code, price) values ('Converse: All Stars', 'De All Starts bestaan al lang, het is een basic en veel gedragen omdat het bij elke outfit past.', '693155505-7', 90.00);
-insert into products (name, description, code, price) values ('Nubikk', 'Deze Nubikks zijn mooie en simpele schoenen.', '686928463-6', 250.00);
-insert into products (name, description, code, price) values ('Timberlands', 'Timberlands zitten heel lekker en zijn perfect voor de wintermaanden.', '492662523-7', 150.00);
-insert into products (name, description, code, price) values ('Vans', 'Vans staan bekend als skate schoenen maar je kan ze ook makkelijk voor dagelijk gebruik gebruiken.', '392672673-2', 100.00);
-insert into products (name, description, code, price) values ('Tommy Hilfiger', 'Tommy Hilfiger staat bekend om hun kleding maar ze verkopen ook goede en mooie schoenen', '970352673-6', 200.00);
+insert into products (name, description, code, price, merk_id) values ('Nike air force 1', 'Nike air force 1 zijn een van de populairste schoenen van de afgelopen jaren. Ze zijn een goede basic die bij elke outfit past.', '816905633-0', 100.00, 1);
+insert into products (name, description, code, price, merk_id) values ('Nike jordans 1', 'Deze collectie van Nike is gemaakt met Micheal Jorden, een van de bekendste basketballers ter wereld.', '077030122-3', 120.00, 2);
+insert into products (name, description, code, price, merk_id) values ('New Balance 550', 'Deze New Balance schoenen werden voor het eerst uitgebracht in 1989, maar heb een nu een kompleet nieuwe look gekregen.', '445924201-X', 220.00, 3);
+insert into products (name, description, code, price, merk_id) values ('Converse: All Stars', 'De All Starts bestaan al lang, het is een basic en veel gedragen omdat het bij elke outfit past.', '693155505-7', 90.00), 4;
+insert into products (name, description, code, price, merk_id) values ('Nubikk', 'Deze Nubikks zijn mooie en simpele schoenen.', '686928463-6', 250.00, 5);
+insert into products (name, description, code, price, merk_id) values ('Timberlands', 'Timberlands zitten heel lekker en zijn perfect voor de wintermaanden.', '492662523-7', 150.00, 6);
+insert into products (name, description, code, price, merk_id) values ('Vans', 'Vans staan bekend als skate schoenen maar je kan ze ook makkelijk voor dagelijk gebruik gebruiken.', '392672673-2', 100.00, 7);
+insert into products (name, description, code, price, merk_id) values ('Tommy Hilfiger', 'Tommy Hilfiger staat bekend om hun kleding maar ze verkopen ook goede en mooie schoenen', '970352673-6', 200.00, 8);
 
-insert into ratings (rating) values ('4,0');
+insert into ratings (rating) values ('4,0'); 
 insert into ratings (rating) values ('4,2');
 insert into ratings (rating) values ('4,9');
 insert into ratings (rating) values ('4,7');
@@ -69,3 +76,13 @@ insert into reviews (review) values ('Ze zijn natuurlijk super makkelijk te comb
 insert into reviews (review) values ('9 maanden geleden lederen Timberland wandelschoenen gekocht waarvan beide zolen volledig doorgesleten zijn.');
 insert into reviews (review) values ('Heb 3 paar tommys gehad in mijn leven gehad en elke keer kwaliteit om van te huilen.');
 insert into reviews (review) values ('Mooi en passen precies. Staat goed bij alle kleding. De schoenen zitten perfect en zijn heel mooi! Sportief en comfortabel.');
+
+insert into merk (name) values ('nike');
+insert into merk (name) values ('nike');
+insert into merk (name) values ('new balance');
+insert into merk (name) values ('converse');
+insert into merk (name) values ('nubikk');
+insert into merk (name) values ('timberlands');
+insert into merk (name) values ('vans');
+insert into merk (name) values ('tommy hilfiger');
+
