d905c7f009b5b3ab316b600d9acd96850b73d24c
indyvg <112356@emmauscollege.nl>
2021-12-26 12:57:56 +0000

    2 nieuwe producten
    Signed-off-by: indyvg <112356@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -58,6 +58,8 @@ insert into products (name, description, code, price, merk_id) values ('Nubikk',
 insert into products (name, description, code, price, merk_id) values ('Timberlands', 'Timberlands zitten heel lekker en zijn perfect voor de wintermaanden.', '492662523-7', 150.00, 6);
 insert into products (name, description, code, price, merk_id) values ('Vans', 'Vans staan bekend als skate schoenen maar je kan ze ook makkelijk voor dagelijk gebruik gebruiken.', '392672673-2', 100.00, 7);
 insert into products (name, description, code, price, merk_id) values ('Tommy Hilfiger', 'Tommy Hilfiger staat bekend om hun kleding maar ze verkopen ook goede en mooie schoenen', '970352673-6', 200.00, 8);
+insert into products (name, description, code, price, merk_id) values ('Puma', 'Deze puma schoenen zijn erg comfortabel en hebben een retrolook.', '640352233-8', 100.00, 9);
+insert into products (name, description, code, price, merk_id) values ('Adidas Forum', 'Deze schoenen zijn weer helemaal in een zijn een oud model met een nieuwe look.', '910367233-3', 120.00, 10);
 
 insert into ratings (rating) values ('4,0'); 
 insert into ratings (rating) values ('4,2');
@@ -67,6 +69,8 @@ insert into ratings (rating) values ('4,1');
 insert into ratings (rating) values ('3,5');
 insert into ratings (rating) values ('3,4');
 insert into ratings (rating) values ('4,1');
+insert into ratings (rating) values ('3,2');
+insert into ratings (rating) values ('4,4');
 
 insert into reviews (review) values ('Helemaal top! De prijs is wel wat duur, maar ik heb ze eerder gehad en ze gingen uiteindelijk 4 jaar mee.');
 insert into reviews (review) values ('Een tijdloze, comfortabele schoen waar je dol op bent voor je voeten.');
@@ -76,6 +80,8 @@ insert into reviews (review) values ('Ze zijn natuurlijk super makkelijk te comb
 insert into reviews (review) values ('9 maanden geleden lederen Timberland wandelschoenen gekocht waarvan beide zolen volledig doorgesleten zijn.');
 insert into reviews (review) values ('Heb 3 paar tommys gehad in mijn leven gehad en elke keer kwaliteit om van te huilen.');
 insert into reviews (review) values ('Mooi en passen precies. Staat goed bij alle kleding. De schoenen zitten perfect en zijn heel mooi! Sportief en comfortabel.');
+insert into reviews (review) values ('Erg fijne en comfortabele schoenen die lang mee gaan.');
+insert into reviews (review) values ('De forums zijn hele goede schoenen die lekker zitten en bij elke outfit passen.');
 
 insert into merk (name) values ('nike specials');
 insert into merk (name) values ('nike');
@@ -85,6 +91,8 @@ insert into merk (name) values ('nubikk');
 insert into merk (name) values ('timberlands');
 insert into merk (name) values ('vans');
 insert into merk (name) values ('tommy hilfiger');
+insert into merk (name) values ('puma');
+insert into merk (name) values ('adidas');
 
 insert into kleuren (kleur) values ('Wit');
 insert into kleuren (kleur) values ('Wit');
